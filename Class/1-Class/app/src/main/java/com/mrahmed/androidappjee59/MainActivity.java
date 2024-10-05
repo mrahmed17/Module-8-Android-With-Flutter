9f84e21681490626fab8bdddad2c2154b7ec9269
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-10-05 19:07:16 +0600

    Android App

--- Class/1-Class/app/src/main/java/com/mrahmed/androidappjee59/MainActivity.java
+++ Class/1-Class/app/src/main/java/com/mrahmed/androidappjee59/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mrahmed.androidappjee59;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnHome=findViewById(R.id.btnHome);
+
+        btnHome.setOnClickListener(v -> {
+            Intent intent= new Intent(getApplicationContext(), Home.class);
+            startActivity(intent);
+        });
+
     }
 }
