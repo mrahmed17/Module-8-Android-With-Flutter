87be98022d4901234d8b72c7c4a4299637b6457c
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-10-09 19:00:42 +0600

    Logo and Activity

--- Project/IsDBQuizExam/app/src/main/java/com/mrahmed/isdbquizexam/SplashActivity.java
+++ Project/IsDBQuizExam/app/src/main/java/com/mrahmed/isdbquizexam/SplashActivity.java
@@ -8,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.FirebaseApp;
-
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        FirebaseApp.initializeApp(this);
-
     }
-
 }
