39ee2da662f8a4ae6f79da685ee5fec9f971cc9a
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-10-16 19:02:10 +0600

    Upgrade

--- Project/QuizTime/app/build.gradle.kts
+++ Project/QuizTime/app/build.gradle.kts
@@ -46,12 +46,12 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+//    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
 }
--- Project/QuizTime/app/src/main/java/com/mrahmed/quiztime/LeaderActivity.kt
+++ Project/QuizTime/app/src/main/java/com/mrahmed/quiztime/LeaderActivity.kt
@@ -1,28 +1,20 @@
 package com.mrahmed.quiztime
 
 import android.os.Bundle
-import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.mrahmed.quiztime.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-
+class LeaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_leader)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val window: Window=this@MainActivity.window
-        window.statusBarColor= ContextCompat.getColor(this@MainActivity,R.color.gray)
     }
 }
